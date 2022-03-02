79243188237bc9fc8d77edef329a35f925ebe20f
Fredrick-Frank <34188600+Fredrick-Frank@users.noreply.github.com>
2022-03-02 14:44:55 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Func_Delegate
+{
+    //using the lambda expression
+    public class Program
+    {
+        public static void Main()
+        {
+            Func<int> getRandomNumber = () => new Random().Next(1,100);
+
+            Func<int, int, int> Sum = (x,y) => x + y;
+
+            Console.WriteLine(getRandomNumber());
+            Console.WriteLine(Sum(100,50));
+        }
+    }
+}
